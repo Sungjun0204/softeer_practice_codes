eeae57e1b8627316fe20d126eed57f0bbd3468ec
Sungjun Kim <kxt1234.max@gmail.com>
2024-01-29 20:08:10 +0900

    Create [Lv.1] 주행거리 비교하기.cpp

---
+++ [Lv.1] 주행거리 비교하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int A, B = 0;
+    
+    cin >> A >> B;
+    if (A > B){ cout << "A" << endl; }
+    else if (B > A) {cout << "B" << endl;}
+    else cout << "same" << endl;
+
+   return 0;
+}
