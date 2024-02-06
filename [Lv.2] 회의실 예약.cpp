b6dc552ea7d3b62537c78d684f0fbbfee3135e98
Sungjun Kim <kxt1234.max@gmail.com>
2024-02-06 22:34:50 +0900

    Update [Lv.2] 회의실 예약.cpp
    9 10, 11 12라고 했을 때, 10-11시간대가 비어있다는 걸 검사 못 함

--- [Lv.2] 회의실 예약.cpp
+++ [Lv.2] 회의실 예약.cpp
@@ -19,6 +19,7 @@ struct meeting{
     int time[10] = {0,};    // 예약 시간 여부 저장
     int pos_start[10] = {0,};   // 예약 가능한 시작 시간
     int pos_end[10] = {0,};   // 예약 가능한 종료 시간
+    int avail_time = 0;       // 예약 가능한 시간대 갯수
 };
 
 struct booked{
@@ -29,8 +30,8 @@ struct booked{
 
 int main(int argc, char** argv)
 {
-    int N = 0;   // 회의실의 갯수
-    int M = 0;   // 예약된 회의의 수
+    int N = 0;   // 회의실의 갯수(최대 50)
+    int M = 0;   // 예약된 회의의 수(최대 100)
     char index[50][1];   // 오름차순으로 출력하기 위한 인덱스 관련 배열변수
 
     meeting room[50] = {0,};    // 회의실 정보를 저장할 구조체 변수 선언
@@ -72,52 +73,118 @@ int main(int argc, char** argv)
         }
     }
 
-    int dash = 0;         // 구분선을 N-1번만 출력하게끔 설정하는 flag변수
-    int pos_time = 0;     // available 횟수를 출력하기 위한 flag 변수
+  
+    
     int num = 2;          // 0, 1을 조건에 맞게 스위칭하기 위해 사용되는 변수
     int switchnum = 0;    // num % 2 값을 저장하기 위한 변수
     // 예약 가능한 시간 계산
     for(int i=0; i<N; i++){
-        cout << "Room " << room[i].name << ":" << endl;   // 양식 출력
-            
+
+         int pos_time = 0;     // available 횟수를 출력하기 위한 flag 변수
+        
          for(int k=0; k<10; k++){
             switchnum = num % 2;   // 나머지 연산으로 0, 1 계속 바꾸게끔
-            int sum=0;
-            for(int a=0; a<10; a++)
+            int sum=0;          
+            for(int a=0; a<10; a++)     // 타임테이블의 모든 값을 다 더함 (풀예약 혹은 프리 상태인지 확인하기 위함)
                 sum += room[i].time[a];
+            
+             // 만일 회의실 예약이 꽉 차면 해당 문구 출력하고 다음 회의실 분석으로
             if(sum == 10){
-                cout << "Not available" << endl;  // 만일 회의실 예약이 꽉 차면 해당 문구 출력하고 다음 회의실 분석으로
                 break;
             }
+
+            // 회의실 예약이 하나도 없을 때 출력
             else if(sum == 0){
-                cout << "1 available:" << endl << "09-18" << endl;  // 회의실 예약이 하나도 없을 때 출력
+                room[i].pos_start[pos_time] = 9;
+                room[i].pos_end[pos_time++] = 18;
                 break;
             }
+
+            // 마지막 시간대만 비어있는 경우
+            else if(k == 9 && room[i].time[9]==0 && sum == 9){
+                room[i].pos_start[pos_time] = 17;
+                room[i].pos_end[pos_time++] = 18;
+                break;
+            }
+                
+                
+            /// 나머지 경우 (대부분의 경우) ///
             else if(room[i].time[k] == switchnum){
-                if(k == 0) cout << "09";
-                //else if(k == 9 && room[i].time[9] == 0 ) cout << "18" << endl; 
+                if(k == 0) room[i].pos_start[pos_time] = 9;
                 else {
-                    if(switchnum == 0) {
-                        cout << k+9-1;
-                        
-                    }
-                    else if(switchnum == 1) cout << "-" << k+9 << endl;
+                    if(switchnum == 0) room[i].pos_start[pos_time] = k+9-1;
+                    else if(switchnum == 1) room[i].pos_end[pos_time++] = k+9;  
                 }
                 num++;
             }
-            else if(k==9 && room[i].time[9]==0) cout << "-18" << endl;
+
+
+                
+            else if(k==9 && room[i].time[9]==0) room[i].pos_end[pos_time++] = 18; 
             
             else continue;
         }
 
-        if(dash++ < N-1) cout << "-----" << endl;   // 마지막은 구분선 출력하지 않도록 설정
+        room[i].avail_time = pos_time;
+    }
+
+
+    //// 최종 출력 ////
+
+    /// 오름차순으로 출력하기 위해 index를 sorting ///
+    /*
+        - 각 이름의 첫 글자만 따 와서 입력했던 순서대로 별도 저장. 
+        - 이름 앞 글자들만 저장한 배열을 오름차순으로 정리
+        - 해당 오름차순 배열과 meeting struct의 name[0]을 대조하면서 순서대로 출력
+        
+    */
+    for(int i=0; i<N; i++)
+        index[i][0] = room[i].name[0];
+
+    char temp;
+    for(int i=0; i<N-1; i++)
+        for(int j=1; j<N; j++){
+            if(index[i][0] > index[j][0]){
+                temp = index[j][0];
+                index[j][0] = index[i][0];
+                index[i][0] = temp;
+            }
+            else continue;
+        }
+
+    int dash = 0;         // 구분선을 N-1번만 출력하게끔 설정하는 flag변수
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            if(index[i][0] == room[j].name[0]){    // 오름차순으로 출력 시작
+                // 방 이름 출력
+                cout << "Room " << room[j].name << ":" << endl;  
+
+                // available 횟수 및 예약 가능 시간대 출력
+                if(room[j].avail_time == 0) cout << "Not available" << endl; 
+                else {
+                    cout << room[j].avail_time << " available:" << endl;
+                    for(int a=0; a<room[j].avail_time; a++){
+                        // 9가 출력이 되면 09로 출력하는 예외처리
+                        if(a == 0 && room[j].pos_start[a] == 9) cout << "09";
+                        else cout << room[j].pos_start[a];
+
+                        // 시간 사이의 dash 및 끝 시간대 출력
+                        cout << "-";
+                        cout << room[j].pos_end[a] << endl;
+
+                    }
+                }
+
+            }
+                
+            else continue;
+
+            if(dash++ < N-1) cout << "-----" << endl;   // 마지막은 구분선 출력하지 않도록 설정
+        }
     }
     
 
     return 0;
 }
-
-
-
 
 
