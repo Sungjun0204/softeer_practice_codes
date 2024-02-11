e108a9b9955ecde5567c1dec73c06d95a3df1907
Sungjun Kim <kxt1234.max@gmail.com>
2024-02-12 00:29:23 +0900

    solved

--- [Lv.2]/[Lv.2] 비밀 메뉴.cpp
+++ [Lv.2]/[Lv.2] 비밀 메뉴.cpp
@@ -3,6 +3,11 @@
         M: 비밀코드 길이
         N: 사용자가 입력한 버튼 횟수
         K: 누른 버튼의 최대 숫자
+    ** 내가 고전했던 내용
+        - 비밀코드 본격 알고리즘 시작하는 부분
+        - 비밀코드, 입력코드 모두 같은 1개의 버튼일 때, 반복문이 실행이 안 되었음
+        - 알고보니 N-M이면 위와 같은 상황은 0이 되므로 반복문 자체를 실행할 수 없게 됨
+        - 따라서 +1을 해 준다
 */
 
 #include<iostream>
@@ -23,15 +28,16 @@ int main(int argc, char** argv)
 
     for(int j=0; j<N; j++)
         cin >> input[j];           // 내가 입력한 커맨드 저장
-
+    
+    
     // 비밀코드 확인 알고리즘 시작
-    for(int j=0; j<N-M; j++){
+    for(int j=0; j<N-M+1; j++){
         if(secret_code[0] == input[j]){
             for(int i=0; i<M; i++){
                 if(secret_code[i] == input[j+i]) flag++;
                 else {flag = 0; break;}
             }
-            
+
             if(flag == M){
                 cout << "secret" << endl;
                 break;
@@ -41,9 +47,8 @@ int main(int argc, char** argv)
         else continue;
     }
 
-    if(flag == 0) cout << "normal" << endl;
+    if(flag != M) cout << "normal" << endl;
     
 
    return 0;
 }
-
