e18fb047bd52ce0ef0844d8dc1e8bb6331956266
Sungjun Kim <kxt1234.max@gmail.com>
2024-02-27 22:43:03 +0900

    Uncompleted
    - 적당히 적은 수에서는 작동이 원활함.
    - 그러나 상대적으로 큰 수를 대입하면 값이 틀어짐
    - 모듈러 재귀함수 쪽에서 문제가 있으나 원인을 모르겠음

--- [Lv.2]/[Lv.2] 바이러스.cpp
+++ [Lv.2]/[Lv.2] 바이러스.cpp
@@ -14,14 +14,16 @@
 
 using namespace std;
 
-int mod_cal(int num, int exp, int mod);         // 2의 거듭제곱 형태의 mod연산을 하는 재귀함수 선언
+int MOD_NUM = 1000000007;       // 최종 mod 연산을 할 숫자 선언
+int temp[100];            // 각 2의 거듭제곱에 따른 모듈러 연산 값을 저장할 배열변수 선언
+int m=0;                  // temp 배열 변수의 index 변수
+int mod_cal(int num, int exp);         // 2의 거듭제곱 형태의 mod연산을 하는 재귀함수 선언
 
 int main(int argc, char** argv)
 {
     int K, P, N = 0;
     int left_part = 0;         // K mod 1000000007 값을 저장할 변수 선언
     int right_part = 0;        // (P^N) mod 1000000007 값을 저장할 변수 선언
-    int mod_num = 1000000007;  // 최종 mod 연산을 할 숫자 선언
 
     cin >> K >> P >> N;        // 주요 변수 입력받음
 
@@ -30,13 +32,13 @@ int main(int argc, char** argv)
     // K*(P^N) mod 1000000007 -> ((K mod 1000000007) * ((P^N) mod 1000000007)) mod 1000000007
     
     // 1. K mod 1000000007 계산
-    left_part = K % mod_num;
+    left_part = K % MOD_NUM;
 
     // 2. (P^N) mod 1000000007 계산
     // 2.1. N을 이진수로 변경
     int div = 0;               // 몫을 저장할 변수 선언
-    int bit_arr[100] = {0,};   // N의 이진수를 저장할 배열변수 선언
-    int k=0;                   // bit_arr의 index 변수 선언
+    int bit_arr[10000] = {0,}; // N의 이진수를 저장할 배열변수 선언
+    int k=0;                   // bit_arr의 index 변수 선언(동시에 이진수의 길이 값을 저장))
 
     div = N;                   // 몫이 계속 작아질 것이므로 N을 대입
     while(div != 1){           // 몫이 1이 될 때까지 반복
@@ -44,23 +46,23 @@ int main(int argc, char** argv)
         div /= 2;                          // 몫 계산
         if(div == 1)bit_arr[k++] = 1;      // 이진수 변환 시의 마지막 1을 대입
     }
-    cout << k << endl;
-    for(int i=0; i<k; i++)
-        cout << bit_arr[i];
-    cout << endl;
+
     
     // 2.2. 2 ≤ B 인 거듭제곱의 mod C 를 계산하고, 그 결과를 순서대로 bit_arr[k]와 곱해서 누산
-    int result=1;                  // 결과 누산 값을 저장할 변수 선언
-    for(int j=0; j<=k; j++)       // 이진수 길이만큼 연산 반복
-        result *= (bit_arr[j] * mod_cal(P, 2^j, mod_num));  // mod 연산 재귀함수 호출 후 누산
-                                                          // 따라서 P^N은 result로 치환 가능
+    long long result=1;                  // 결과 누산 값을 저장할 변수 선언
+    long long tem=0;
+    for(int j=0; j<k; j++){                      // 이진수 길이만큼 연산 반복
+        tem = mod_cal(P, pow(2,j));
+        if(bit_arr[j] == 1) result *= tem;       // mod 연산 재귀함수 호출 후 누산
+        else if(bit_arr[j] == 0) continue;       // 따라서 P^N은 result로 치환 가능
+    }                                                    
 
     // 2.3. right_part 계산
-    right_part = result % mod_num;
+    right_part = result % MOD_NUM;
 
     
     // 3. K*(P^N) mod 1000000007최종 계산
-    cout << (left_part * right_part) % mod_num << endl;
+    cout << (left_part * right_part) % MOD_NUM;
     
     
     return 0;
@@ -68,7 +70,17 @@ int main(int argc, char** argv)
 
 
 // 2의 거듭제곱 형태의 mod연산을 하는 재귀함수 선언
-int mod_cal(int num, int exp, int mod){
-    if(exp == 1) return num % mod;
-    return (mod_cal(num, exp/2, mod) * mod_cal(num, exp/2, mod)) % mod;
+// int mod_cal(int num, int exp){
+//     if(exp == 1)                              // 1제곱이면 그냥 계산
+//         temp[m] = num % MOD_NUM;
+    
+//     else if(exp>=2)                         // 2제곱 이상이면 이전 연산 값을 활용하여 계산
+//         temp[m] = (temp[m-1] * temp[m-1]) % MOD_NUM;
+
+//     return temp[m++];
+// }
+
+int mod_cal(int num, int exp){
+    if(exp == 1) return num % MOD_NUM;
+    else return (mod_cal(num, exp/2) * mod_cal(num, exp/2)) % MOD_NUM;
 }
